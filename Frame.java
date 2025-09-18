d704d8329d76dbcf1a4b132979c8f1c05ec36e95
Babskieee <ronilosepida13@gmail.com>
2025-09-18 12:36:45 +0800

    .

--- Frame.java
+++ Frame.java
@@ -1,9 +1,5 @@
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame {
 
@@ -24,7 +20,7 @@ public class Frame extends JFrame {
         //Label
         lbl = new JLabel("Given Name: ");
         lbl.setBounds(100, 100, 100, 50);
-        inputPanel= new JPanel(new GridLayout(5, 2, 10, 10));
+        inputPanel = new JPanel(new GridLayout(5, 2, 10, 10));
         inputPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         fName = new JTextField();
