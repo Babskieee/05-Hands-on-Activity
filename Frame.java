fd97745353849ae83e52fcd84ad31832d5ac8b68
Babskieee <ronilosepida13@gmail.com>
2025-09-18 11:55:37 +0800

    Set up frame

---
+++ Frame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+    
+    Frame(){
+        this.setResizable(false);
+        this.setResizable(false);
+
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        
+        
+        this.setVisible(true);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-import javax.swing.JFrame;
-
-public class Main extends JFrame{
+public class Main{
     public static void main(String[] args) {
-        
+        new Frame();
     }
 }
