94e751e0e9f143d9917d004f166693268c763c58
Babskieee <ronilosepida13@gmail.com>
2025-09-19 21:48:26 +0800

    // Main file is deleted // main method is in Frame file // can print the data

--- Frame.java
+++ Frame.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Frame extends JFrame {
 
@@ -10,14 +11,17 @@ public class Frame extends JFrame {
     Frame(){
         this.setResizable(false);
 
-        //Main Frame
+        // Main Frame
         this.setTitle("05 Hands-on Activity");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
 
+        // Input fields design
         inputPanel = new JPanel(new GridLayout(4, 2, 10, 10));
         inputPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        
+        // Input fields
         inputPanel.add(new JLabel("Given Name:"));
         inputPanel.add(fName = new JTextField());
         inputPanel.add(new JLabel("Middle Name:"));
@@ -28,19 +32,52 @@ public class Frame extends JFrame {
         inputPanel.add(uEmail = new JTextField());
         inputPanel.setBounds(50, 50, 400, 200);
 
-        //Button
+        // Button
         btn = new JButton("Submit");
         btn.setBounds(190, 300, 100, 50);
 
+        // Button response
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Get inputs
+                String given = fName.getText();
+                String middle = mName.getText();
+                String last = lName.getText();
+                String email = uEmail.getText();
+
+                // Remove the input fields
+                remove(inputPanel);
+                remove(btn);
+
+                // New panel for output
+                JPanel outputPanel = new JPanel(new GridLayout(5, 1, 10, 10));
+                outputPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+                outputPanel.add(new JLabel("Given Name: " + given));
+                outputPanel.add(new JLabel("Middle Name: " + middle));
+                outputPanel.add(new JLabel("Last Name: " + last));
+                outputPanel.add(new JLabel("Email: " + email));
+
+                outputPanel.setBounds(50, 100, 400, 200);
+                add(outputPanel);
+
+                // Refresh for output
+                revalidate();
+                repaint();
+            }
+        });
 
 
+        //Elements
         this.add(inputPanel);
-        //Adds button
         this.add(btn);
 
         //Whole form is visible
         this.setVisible(true);
+    }
 
-        
+    public static void main(String[] args) {
+        new Frame();
     }
 }
